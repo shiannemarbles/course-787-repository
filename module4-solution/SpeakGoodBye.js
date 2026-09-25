ab5643066ae0d1f97af9eb2a22ae3f3a70ac7d5b
Shianne Marbley <Shianne.Marbley@jhuapl.edu>
2026-09-25 13:58:08 -0400

    steps 1-6

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -3,7 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
-
+(function () {
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -23,3 +23,4 @@ function speak(name) {
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+})();
